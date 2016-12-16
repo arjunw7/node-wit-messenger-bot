6d2d3aec57b9372bde67a2ae10a074b35bb6e16e
Arjun Wadhwa <arjunw7@gmail.com>
2016-12-16 11:32:20 +0530

    initial_deploy

---
+++ wit/messenger.js
@@ -0,0 +1,277 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const express = require('express');
+const fetch = require('node-fetch');
+const request = require('request');
+
+var mongojs = require("mongojs");
+var db = mongojs('mongodb://arjunw7:13bcb0062@ds129038.mlab.com:29038/salesbot', ['sales']);
+
+let Wit = null;
+let log = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  log = require('../').log;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  log = require('node-wit').log;
+}
+
+// Webserver parameter
+const PORT = process.env.PORT || 8445;
+
+// Wit.ai parameters
+const WIT_TOKEN = 'TIT545YKEJHN7QB7HJHSTWP34EZSS7YS';
+
+// Messenger API parameters
+const FB_PAGE_TOKEN = 'EAAFHizhSqiYBALxlZADQRG2Ie0cApCKpLCOMSofYlKkS908vZCTXZC1helVJVlnSwEaqhS6Tj9vj3VQwFP20DHYE1MUXws284OcqR8CCEhZCoCVrZAnZAS9XTk4nEjIHZBXmYbSBI2ird3QafuVduneEUp4b3g2NFzG46rZCc8IcdQZDZD';
+if (!FB_PAGE_TOKEN) { throw new Error('missing FB_PAGE_TOKEN') }
+const FB_APP_SECRET = '897f48a01ec53d19d02dfdb55cff0c4f';
+if (!FB_APP_SECRET) { throw new Error('missing FB_APP_SECRET') }
+
+let FB_VERIFY_TOKEN = arjunisawesome;
+
+const fbMessage = (id, text) => {
+  const body = JSON.stringify({
+    recipient: { id },
+    message: { text },
+  });
+  const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
+  return fetch('https://graph.facebook.com/me/messages?' + qs, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body,
+  })
+  .then(rsp => rsp.json())
+  .then(json => {
+    if (json.error && json.error.message) {
+      throw new Error(json.error.message);
+    }
+    return json;
+  });
+};
+
+const sessions = {};
+
+const findOrCreateSession = (fbid) => {
+  let sessionId;
+  // Let's see if we already have a session for the user fbid
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].fbid === fbid) {
+      // Yep, got it!
+      sessionId = k;
+    }
+  });
+  if (!sessionId) {
+    // No session found for user fbid, let's create a new one
+    sessionId = new Date().toISOString();
+    sessions[sessionId] = {fbid: fbid, context: {}};
+  }
+  return sessionId;
+};
+
+var firstEntityValue = (entities, entity) => {
+  var val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
+// Our bot actions
+const actions = {
+  send({sessionId}, {text}) {
+    // Our bot has something to say!
+    // Let's retrieve the Facebook user whose session belongs to
+    const recipientId = sessions[sessionId].fbid;
+    if (recipientId) {
+      console.log(sessionId);
+      // Yay, we found our recipient!
+      // Let's forward our bot response to her.
+      // We return a promise to let our bot know when we're done sending
+      return fbMessage(recipientId, text)
+      .then(() => null)
+      .catch((err) => {
+        console.error(
+          'Oops! An error occurred while forwarding the response to',
+          recipientId,
+          ':',
+          err.stack || err
+        );
+      });
+    } else {
+      console.error('Oops! Couldn\'t find user for session:', sessionId);
+      // Giving the wheel back to our bot
+      return Promise.resolve()
+    }
+  },
+  getSales({sessionId, text, context, entities}) {
+    return new Promise(function(resolve, reject){
+      var exceptDate = firstEntityValue(entities, 'exceptDate');
+      var datetime = firstEntityValue(entities, 'datetime');
+      console.log(entities);
+      if(datetime){
+        var totalSales;
+            if(exceptDate){
+                db.sales.find({ dateSold: { $ne: entities.datetime[0].value} }).count(function (err, res) {
+                    context.unitsSold = res;
+                    context.maxDate = '06-12-2016';
+                    delete context.missingDate;
+                    return resolve(context);
+                  });
+            }
+            else{
+                db.sales.find({ dateSold:entities.datetime[0].value }).count(function (err, res) {
+                    context.unitsSold = res;
+                    context.maxDate = '06-12-2016';
+                    delete context.missingDate;
+                    return resolve(context);
+                  });
+            }
+        }
+        else
+        { 
+          context.missingDate = true;
+          delete context.unitsSold;
+          delete context.maxdate;
+          return resolve(context);  
+        }
+    });  
+  },
+  emptyContext({context}){
+        delete context.unitsSold;
+        delete context.maxdate;
+        delete context.missingDate;
+  }
+};
+
+// Setting up our bot
+const wit = new Wit({
+  accessToken: WIT_TOKEN,
+  actions,
+  logger: new log.Logger(log.INFO)
+});
+
+// Starting our webserver and putting it all together
+const app = express();
+app.use(({method, url}, rsp, next) => {
+  rsp.on('finish', () => {
+    console.log(`${rsp.statusCode} ${method} ${url}`);
+  });
+  next();
+});
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+
+// Webhook setup
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+// Message handler
+app.post('/webhook', (req, res) => {
+  // Parse the Messenger payload
+  // See the Webhook reference
+  // https://developers.facebook.com/docs/messenger-platform/webhook-reference
+  const data = req.body;
+
+  if (data.object === 'page') {
+    data.entry.forEach(entry => {
+      entry.messaging.forEach(event => {
+        if (event.message && !event.message.is_echo) {
+          // Yay! We got a new message!
+          // We retrieve the Facebook user ID of the sender
+          const sender = event.sender.id;
+
+          // We retrieve the user's current session, or create one if it doesn't exist
+          // This is needed for our bot to figure out the conversation history
+          const sessionId = findOrCreateSession(sender);
+
+          // We retrieve the message content
+          const {text, attachments} = event.message;
+
+          if (attachments) {
+            // We received an attachment
+            // Let's reply with an automatic message
+            fbMessage(sender, 'Sorry I can only process text messages for now.')
+            .catch(console.error);
+          } else if (text) {
+            // We received a text message
+
+            // Let's forward the message to the Wit.ai Bot Engine
+            // This will run all actions until our bot has nothing left to do
+            wit.runActions(
+              sessionId, // the user's current session
+              text, // the user's message
+              sessions[sessionId].context // the user's current session state
+            ).then((context) => {
+              // Our bot did everything it has to do.
+              // Now it's waiting for further messages to proceed.
+              console.log('Waiting for next user messages');
+
+              // Based on the session state, you might want to reset the session.
+              // This depends heavily on the business logic of your bot.
+              // Example:
+              // if (context['done']) {
+              //   delete sessions[sessionId];
+              // }
+
+              // Updating the user's current session state
+              sessions[sessionId].context = context;
+            })
+            .catch((err) => {
+              console.error('Oops! Got an error from Wit: ', err.stack || err);
+            })
+          }
+        } else {
+          console.log('received event', JSON.stringify(event));
+        }
+      });
+    });
+  }
+  res.sendStatus(200);
+});
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', FB_APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
+
+app.listen(PORT);
+console.log('Listening on :' + PORT + '...');
